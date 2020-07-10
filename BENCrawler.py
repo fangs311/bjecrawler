d196714dc4072707ce4d0ab499cef8be7dc72ee9
fangs311 <35051459+fangs311@users.noreply.github.com>
2020-07-10 08:16:49 -0400

    Update BENCrawler.py

--- BENCrawler.py
+++ BENCrawler.py
@@ -1,20 +1,24 @@
 #!/usr/bin/python
+
 """
-Created on Fri Jun 10 11:22:02 2020
+Created on Fri July 10 11:22:02 2020
 @author: Zefang
 """
+
 import urllib3
-from os import listdir
+from os import getcwd
 from os import remove
 from datetime import datetime
 from datetime import timedelta
 from PyPDF2 import PdfFileReader, PdfFileWriter
+
 import warnings
 warnings.resetwarnings()
-warnings.filterwarnings('ignore')
+warnings.filterwarnings('ignore')#supress PdfFileReader warnings
 
+#download pages and merge into single pdf
 def download_newspaper(date):
-    path = '/volume1/book/Journals/BeijingEveningNews/'
+    path = getcwd()
     url1 = "http://bjwb.bjd.com.cn/images/"
     pages = [str(x).zfill(2) for x in range(1,100)]    
     filename = "BJENewspaper_" + date.replace('/','').replace('-','') + ".pdf"
@@ -31,27 +35,28 @@ def download_newspaper(date):
         response = http.request('GET', url, preload_content=False)
         print("try download " + date + " Page " + page)
         
-        if (len(response.data) < 2**13): #fail 5.1K
+        if (len(response.data) < 2**13): #empty page if less than 5.1K
             break
         with open(path + 'temp/' + temp_page,"wb") as outpage:
             outpage.write(response.data)
         reader = PdfFileReader(path + 'temp/' + temp_page, strict=False)
         writer.addPage(reader.getPage(0))
-        remove(path + 'temp/' + temp_page)
+        remove(path + 'temp/' + temp_page)#clear temp depository
     with open(filename, 'wb') as outfile:
         writer.write(outfile)
     print("Completed "+filename)
         
-def main_history():
+def main_history(): #use to pull archieve from 2019-01-01
     today = datetime.today()
     while(today.date() > datetime(2018 , 12, 31).date()):
         today = today - timedelta(days=1)
         date = today.strftime('%Y-%m/%d')
         download_newspaper(date)
                 
-def main():
+def main(): #run with cronjob for daily update
     today = datetime.today()
     date = today.strftime('%Y-%m/%d')
     download_newspaper(date)
+
 if __name__ == "__main__":
     main()
